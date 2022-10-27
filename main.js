3b572fcb42e8672b5644a04d070526978bfed250
CarmenPatriciaPerezVarelaADSO <patriciaperezvarela@gmail.com>
2022-10-26 20:54:04 -0500

    Taller 1-Conceptos basicos parte1

---
+++ main.js
@@ -0,0 +1,15 @@
+// "Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos 
+// entre el 1 y el 1O, es decir, 1 + 2 + 3 + .... + 1O."
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4
+let e = 5;
+let f = 6;
+let g = 7;
+let h = 8;
+let i = 9;
+let j = 10;
+
+console.log(`la suma es:`,a+b+c+d+e+f+g+h+i+j);
