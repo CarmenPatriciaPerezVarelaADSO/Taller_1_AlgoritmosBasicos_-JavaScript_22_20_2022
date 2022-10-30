84c883770f6ddb406b5bd822932a3d444f03560a
CarmenPatriciaPerezVarelaADSO <patriciaperezvarela@gmail.com>
2022-10-29 19:41:34 -0500

    Solucion ejercicio 2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// "Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos 
+// "1. Desarrolle un algoritmo que realice la sumatoria de los números enteros comprendidos 
 // entre el 1 y el 1O, es decir, 1 + 2 + 3 + .... + 1O."
 
 let a = 1;
 let b = 2;
 let c = 3;
-let d = 4
+let d = 4;
 let e = 5;
 let f = 6;
 let g = 7;
@@ -13,3 +13,10 @@ let i = 9;
 let j = 10;
 
 console.log(`la suma es:`,a+b+c+d+e+f+g+h+i+j);
+
+
+// "2. Determinar la hipotenusa de un triángulo rectángulo conocidas las longitudes de sus dos catetos. 
+// Desarrolle el algoritmo correspondiente. Formula: (HIP = √CATa + √CATb)."
+
+console.log(`la hipotenusa de un triángulo rectángulo conocidas 
+las longitudes de sus dos catetos e=5 y f=6 es:`,Math.hypot(5,6));
